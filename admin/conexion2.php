100a268f49f0cfcea6ddb0f0b3e8f41a380c8242
lordeder <87275538+lordeder@users.noreply.github.com>
2025-06-05 17:11:24 -0500

    Update conexion2.php

--- admin/conexion2.php
+++ admin/conexion2.php
@@ -3,12 +3,12 @@
 
 function conectarse2()
 {
-        if (!($link2 = new mysqli('localhost', 'root', 'ederunt006'))) { //Ederunt006@
+        if (!($link2 = new mysqli('localhost', 'root', 'youruser'))) { //
             echo "Error conectando al servidor de base de datos principal."; 
             exit(); 
         }
         
-        if (!($link2->select_db('g4mersclub3'))) { //g4mersclub
+        if (!($link2->select_db('yourdb'))) { //
             echo "Error seleccionando la base de datos de colegios."; 
             exit(); 
         }
